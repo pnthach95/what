eb9c0ca4586f1578cc16b8934926aec4901fe420
Pham Ngoc Thach <thachxyz123@gmail.com>
2024-04-14 10:30:07 +0700

    sample-package

---
+++ sample-package/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Sample = () => {
+  return <Text style={{color: 'red', fontWeight: 'bold'}}>Sample code</Text>;
+};
+
+export default Sample;
